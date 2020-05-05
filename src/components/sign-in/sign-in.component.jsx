460caf03bc80f603564c4fcfaa7e1ef4ee72b1f0
davidkartuzinski <david@kaidawei.me>
2020-05-05 12:21:07 +0200

    Add user to firebase database and check to see if we have user in db

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,7 +49,7 @@ class SignIn extends React.Component {
             label='Password'
             required
           />
-          <div class='buttons'>
+          <div className='buttons'>
             <CustomButton type='submit'> Sign In </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign In with Google
